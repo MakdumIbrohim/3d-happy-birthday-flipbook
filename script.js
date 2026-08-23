5d7dec50d0f59114837edd16abcb6f2d6660aa3d
MakdumIbrohim <makdumibrohim3@gmail.com>
2026-08-24 06:38:06 +0700

    feat: nambah page 7 isi pesan ultah lebih panjang + update logika flipbook

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const page4 = document.getElementById('page4');
     const page5 = document.getElementById('page5');
     const page6 = document.getElementById('page6');
-    let t1, t2, t3, t4;
+    const page7 = document.getElementById('page7');
+    let t1, t2, t3, t4, t5;
 
     // Buka Halaman
     btnOpen.addEventListener('click', () => {
@@ -16,18 +17,21 @@ document.addEventListener('DOMContentLoaded', () => {
         t2 = setTimeout(() => { page3.classList.add('flipped'); }, 7000);
         t3 = setTimeout(() => { page4.classList.add('flipped'); }, 11000);
         t4 = setTimeout(() => { page5.classList.add('flipped'); }, 15000);
+        t5 = setTimeout(() => { page6.classList.add('flipped'); }, 19000);
     });
 
-    // Reset di halaman terakhir (6)
-    page6.addEventListener('click', () => {
+    // Reset di halaman terakhir (7)
+    page7.addEventListener('click', () => {
         page1.classList.remove('hidden');
         page2.classList.remove('flipped');
         page3.classList.remove('flipped');
         page4.classList.remove('flipped');
         page5.classList.remove('flipped');
+        page6.classList.remove('flipped');
         clearTimeout(t1);
         clearTimeout(t2);
         clearTimeout(t3);
         clearTimeout(t4);
+        clearTimeout(t5);
     });
 });
